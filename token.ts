0425eac803e364ed4009e83ec4baa6c210cfe54d
sovietbehemoth <73245562+sovietbehemoth@users.noreply.github.com>
2021-04-04 00:37:47 -0400

    Create token.ts

---
+++ token.ts
@@ -0,0 +1,15 @@
+//Define your token here, defining here will set your token globally
+const token = "TOKEN";
+
+//Set as false if you are using a user account
+let bot: string = "true";
+
+let headers: any;
+if (bot === "false") headers = {
+    "Authorization": token,
+    "Content-Type": "application/json"
+}; else if (bot === "true") headers = {
+    "Authorization": `Bot ${token}`,
+    "Content-Type": "application/json"
+}
+export { headers, bot, token };
